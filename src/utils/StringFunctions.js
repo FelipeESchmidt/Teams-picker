b33b1c40a1914304148aa71c0662249a4f2ce0c2
Felipe Schmidt <schmidtfe2@gmail.com>
2022-01-29 12:13:07 -0300

    creating generateRandomId function

--- src/utils/StringFunctions.js
+++ src/utils/StringFunctions.js
@@ -1,2 +1,5 @@
 export const capitalizeFirstLetter = ([first, ...rest]) =>
-    first.toLocaleUpperCase() + rest.join('')
+    first.toLocaleUpperCase() + rest.join('');
+
+export const generateRandomId = () =>
+    Math.random().toString(36).substr(2, 22);
