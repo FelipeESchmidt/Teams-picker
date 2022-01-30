1a373ef6bc35382bbac764a7b0b49bed0dd19476
Felipe Schmidt <schmidtfe2@gmail.com>
2022-01-30 14:41:32 -0300

    adapting Menu to responsive

--- src/view/Menu/index.styles.js
+++ src/view/Menu/index.styles.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 export const StyledLogo = styled.h2`
   font-size: 30px;
   color: #666;
+  @media screen and (max-width: 950px) {
+    font-size: 25px;
+  }
 `;
 
 export const StyledMenu = styled.div`
@@ -10,10 +13,15 @@ export const StyledMenu = styled.div`
   flex-direction: column;
   width: 30%;
   margin: 40px;
+  padding: 40px;
   border: 2px solid #212121;
   box-shadow: 0 3px 8px 0 #232323;
   border-radius: 20px;
-  padding: 40px;
+  @media screen and (max-width: 950px) {
+    width: 98%;
+    margin: 1%;
+    padding: 10px;
+  }
 `;
 
 export const StyledInputs = styled.div`
