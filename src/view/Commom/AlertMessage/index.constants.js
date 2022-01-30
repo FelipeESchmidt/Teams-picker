e5df1afa4cd3845f3e287e3b5d91e90a3efde823
Felipe Schmidt <schmidtfe2@gmail.com>
2022-01-30 02:04:45 -0300

    creating colors constants to Alert types

---
+++ src/view/Commom/AlertMessage/index.constants.js
@@ -0,0 +1,18 @@
+export const alertColors = {
+    "alert": {
+        color: "black",
+        background: "#ffee58",
+    },
+    "error": {
+        color: "black",
+        background: "#ef5350",
+    },
+    "success": {
+        color: "black",
+        background: "#66bb6a",
+    },
+    "info": {
+        color: "black",
+        background: "#29b6f6",
+    },
+}
