057dafcd00d130d48f349120914da32a1936a083
Felipe Schmidt <schmidtfe2@gmail.com>
2022-01-30 02:08:10 -0300

    adding funtions to help copying text to clipboard

--- src/utils/AppFunctions.js
+++ src/utils/AppFunctions.js
@@ -1,6 +1,28 @@
+const separator = `\n********************\n`;
+
 export const createMessage = (text, type) => {
     return {
         message: text,
         type
     }
+}
+
+const copyToClip = (text) => {
+    navigator.clipboard.writeText(text);
+}
+
+const teamToString = (team) => {
+    const teamText = team.map((player, index) => `${index + 1} - ${player.name}`).join('\n');
+    const text = separator.concat(teamText).concat(separator);
+    return text;
+}
+
+export const copyToClipBoardAllTeams = (teams) => {
+    const text = teams.map(team => teamToString(team)).join('\n');
+    copyToClip(text);
+}
+
+export const copyToClipBoardSingleTeam = (team) => {
+    const text = teamToString(team);
+    copyToClip(text);
 }
