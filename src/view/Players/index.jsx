3e7357245930fb59191de8a9129e8c4991bb4f27
Felipe Schmidt <schmidtfe2@gmail.com>
2022-01-30 02:09:35 -0300

    Players component now use redux to get players info

--- src/view/Players/index.jsx
+++ src/view/Players/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { selectPlayers } from '../../store/Players/Players.selectors';
 
 import Player from './Player';
 import { StyledWrapper } from './index.styles';
 
-function Players({ players }) {
+function Players() {
+    const { players } = useSelector(selectPlayers);
+
     return (
         <StyledWrapper>
             {players.map(player => (
