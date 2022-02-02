0e63a22125dd8df64d7bce925176a0481b5f0dc1
Felipe Schmidt <schmidtfe2@gmail.com>
2022-02-01 23:09:21 -0300

    adding Options reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,13 @@ import { createStore, combineReducers } from 'redux';
 import teamsReducer from './Teams/Teams.reducer';
 import playersReducer from './Players/Players.reducer';
 import alertReducer from './Alert/Alert.reducer';
+import optionsReducer from './Options/Options.reducer';
 
 const rootReducer = combineReducers({
     teams: teamsReducer,
     players: playersReducer,
     alert: alertReducer,
+    options: optionsReducer,
 });
 
 export const store = createStore(rootReducer);
