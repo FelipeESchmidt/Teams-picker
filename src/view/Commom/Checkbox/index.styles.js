4c0d93dbb59a51c7192e82776f2349659d201fca
Felipe Schmidt <schmidtfe2@gmail.com>
2022-02-01 23:49:29 -0300

    bettering Checkbox styles

--- src/view/Commom/Checkbox/index.styles.js
+++ src/view/Commom/Checkbox/index.styles.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const StyledWrapper = styled.div`
     display: flex;
+    align-items: center;
     gap: 10px;
     color: #666;
     cursor: pointer;
@@ -12,12 +13,14 @@ export const StyledCheckbox = styled.div`
     position: relative;
     top: 0;
     left: 0;
+    width: 22px;
+    height: 22px;
     background-color: white;
     box-shadow: inset 0px 0px 0px 2px #171818;
 `;
 
 export const StyledIcon = styled(FontAwesomeIcon)`
-    width: 22px;
+    width: 22px !important;
     height: 22px;
     font-size: 22px;
     
