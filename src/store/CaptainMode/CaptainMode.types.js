cdb75c346474237fd5cfddc4312e5ee75805a06e
Felipe Schmidt <schmidtfe2@gmail.com>
2022-02-04 00:08:40 -0300

    creating captainMode types

---
+++ src/store/CaptainMode/CaptainMode.types.js
@@ -0,0 +1,3 @@
+export const START_SELECTION = '@captainMode/START_SELECTION';
+export const SELECT_CAPTAIN = '@captainMode/SELECT_CAPTAIN';
+export const SELECT_PLAYER = '@captainMode/SELECT_PLAYER';
