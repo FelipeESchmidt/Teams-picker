18d6d24f1c148bc5e76f20b9fe67270ae0632fd1
Felipe Schmidt <schmidtfe2@gmail.com>
2022-02-04 00:08:24 -0300

    adding captainMode to store

--- src/store/store.js
+++ src/store/store.js
@@ -4,12 +4,14 @@ import teamsReducer from './Teams/Teams.reducer';
 import playersReducer from './Players/Players.reducer';
 import alertReducer from './Alert/Alert.reducer';
 import optionsReducer from './Options/Options.reducer';
+import captainModeReducer from './CaptainMode/CaptainMode.reducer';
 
 const rootReducer = combineReducers({
     teams: teamsReducer,
     players: playersReducer,
     alert: alertReducer,
     options: optionsReducer,
+    captainMode: captainModeReducer,
 });
 
 export const store = createStore(rootReducer);
