e317ffe4156d04ef707251e154b1d01a00e23b95
Felipe Schmidt <schmidtfe2@gmail.com>
2022-02-07 00:33:29 -0300

    developing SelectingCaptains component

--- src/view/CaptainTeams/SelectingCaptains/index.jsx
+++ src/view/CaptainTeams/SelectingCaptains/index.jsx
@@ -1,14 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { selectCaptain } from '../../../store/CaptainMode/CaptainMode.actions';
+import { selectCaptainMode } from '../../../store/CaptainMode/CaptainMode.selectors';
 import CaptainsContext from '../../../context/CaptainsContext';
 
+import SelectablePlayer from '../SelectablePlayer';
+
+import { StyledCaptains, StyledPlayers, StyledWrapper } from './index.styles';
+
 function SelectingCaptains() {
+    const dispatch = useDispatch();
+
+    const { availablePlayers, nTeams } = useSelector(selectCaptainMode);
+
+    const [captains, setCaptains] = useState(nTeams);
     const { nextStep } = useContext(CaptainsContext);
 
+    const handleSelectCaptain = (id) => {
+        dispatch(selectCaptain(id));
+        if (captains > 1) {
+            setCaptains(captains - 1);
+        } else {
+            nextStep();
+        }
+    }
+
     return (
-        <>
-            <p>a</p>
-        </>
+        <StyledWrapper>
+            <StyledCaptains>Selecione mais {captains} {captains !== 1 ? 'capitães' : 'capitão'}</StyledCaptains>
+            <StyledPlayers>
+                {availablePlayers.map((player) => (
+                    <SelectablePlayer key={player.id} player={player} onSelect={handleSelectCaptain} />
+                ))}
+            </StyledPlayers>
+        </StyledWrapper>
     );
 }
 
