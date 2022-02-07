d9e3d5e58179b4772c4f4362026971dd010cf53a
Felipe Schmidt <schmidtfe2@gmail.com>
2022-02-07 00:33:48 -0300

    developing SelectingPlayers component

---
+++ src/view/CaptainTeams/SelectingPlayers/index.styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const StyledWrapper = styled.div`
+    padding-top: 20px;
+`;
+
+export const StyledCaptain = styled.h2`
+    color: #666;
+    margin: 10px 0;
+`;
+
+export const StyledCaptainName = styled.span`
+    text-transform: uppercase;
+`;
+
+export const StyledPlayers = styled.div`
+    display: flex;
+    gap: 10px;
+`;
