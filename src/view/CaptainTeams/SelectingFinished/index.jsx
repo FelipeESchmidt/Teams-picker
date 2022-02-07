bd366e2d77c64ac061f513b34bfad0dc970926c9
Felipe Schmidt <schmidtfe2@gmail.com>
2022-02-07 00:33:39 -0300

    developing SelectingFinished component

--- src/view/CaptainTeams/SelectingFinished/index.jsx
+++ src/view/CaptainTeams/SelectingFinished/index.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { newMessage } from '../../../store/Alert/Alert.actions';
+import { setTeams } from '../../../store/Teams/Teams.actions';
+import { selectCaptainMode } from '../../../store/CaptainMode/CaptainMode.selectors';
+
+import { finalizeSelections } from '../../../store/CaptainMode/CaptainMode.actions';
+import { createMessage } from '../../../utils/AppFunctions';
+
+import Teams from '../../Teams';
+import { StyledWrapper } from './index.styles';
 
 function SelectingFinished() {
+    const [first, setFirst] = useState(true);
+    const dispatch = useDispatch();
+
+    const { teams } = useSelector(selectCaptainMode);
+
+    useEffect(() => {
+        if (!first) return;
+        dispatch(setTeams(teams));
+        const success = createMessage("Seleções finalizadas com sucesso!", "info");
+        dispatch(newMessage(success));
+        dispatch(finalizeSelections());
+        setFirst(false);
+    }, [dispatch, teams, first]);
+
     return (
-        <>
-        </>
+        <StyledWrapper>
+            <Teams />
+        </StyledWrapper>
     );
 }
 
