eb4cbe9d97035313b40f9d1e3966d531fe2c4297
Felipe E. Schmidt <schmidtfe2@gmail.com>
2023-09-21 21:31:56 -0300

    fix: fixing clipboard copy function

--- src/utils/AppFunctions.js
+++ src/utils/AppFunctions.js
@@ -3,35 +3,53 @@ const finalSeparator = `\n********************\n`;
 const teamSeparator = (title) => `------Team ${title}------\n`;
 
 export const createMessage = (text, type) => {
-    return {
-        message: text,
-        type
-    }
-}
+  return {
+    message: text,
+    type,
+  };
+};
 
 const copyToClip = (text) => {
-    try {
-        navigator.clipboard.writeText(text);
-        return createMessage("Copiado com sucesso", "success");
-    }
-    catch (err) {
-        return createMessage("Erro ao copiar", "error");
-    }
-}
+  if (navigator.clipboard && window.isSecureContext) {
+    return navigator.clipboard.writeText(text);
+  } else {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    textArea.style.position = "fixed";
+    textArea.style.left = "-999999px";
+    textArea.style.top = "-999999px";
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    return new Promise((res, rej) => {
+      document.execCommand("copy") ? res("") : rej();
+      textArea.remove();
+    });
+  }
+};
 
 const teamToString = (team, teamTitle) => {
-    const teamText = team.map((player, index) => `${index + 1} - ${player.name}`).join('\n');
-    const text = teamSeparator(teamTitle).concat(initialSeparator).concat(teamText).concat(finalSeparator);
-    return text;
-}
+  const teamText = team
+    .map((player, index) => `${index + 1} - ${player.name}`)
+    .join("\n");
+  const text = teamSeparator(teamTitle)
+    .concat(initialSeparator)
+    .concat(teamText)
+    .concat(finalSeparator);
+  return text;
+};
 
 export const copyToClipBoardAllTeams = (teams, hasCaptain) => {
-    const text = teams.map((team, index) => teamToString(team, hasCaptain ? team[0].name : index + 1)).join('\n');
-    const message = copyToClip(text);
-    return message;
-}
+  const text = teams
+    .map((team, index) =>
+      teamToString(team, hasCaptain ? team[0].name : index + 1)
+    )
+    .join("\n");
+  const message = copyToClip(text);
+  return message;
+};
 
 export const copyToClipBoardSingleTeam = (team) => {
-    const text = teamToString(team);
-    copyToClip(text);
-}
+  const text = teamToString(team);
+  copyToClip(text);
+};
